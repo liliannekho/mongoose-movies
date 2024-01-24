7b042161916b731fa312b908ea60443a12990a21
unknown <liliannekho@gmail.com>
2024-01-23 19:41:47 -0500

    added movie router code

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,1 +1,13 @@
 const Movie = require('../models/movie')
+
+/*
+//POST /movies: Accepst Movie Data and Creates A Movie
+router.post('/', movieCtrol.create)
+//GET /movies: Returns a list of all movies.
+router.get('/', movieCtrol.index)
+//GET /movies/:id: Gets An Individual Movie
+router.get('/:id', movieCtrol.show)
+//POST /performers: Accepts performer data and creates a new performer.
+router.get('/:movieid/performers/:perfmerId', movieCtrl.addPerformer)
+
+*/
--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -2,4 +2,14 @@ const express = require('express')
 const router = express.Router()
 const movieCtrl = require('../controllers/moviesController')
 
+//POST /movies: Accepst Movie Data and Creates A Movie
+router.post('/', movieCtrol.create)
+//GET /movies: Returns a list of all movies.
+router.get('/', movieCtrol.index)
+//GET /movies/:id: Gets An Individual Movie
+router.get('/:id', movieCtrol.show)
+//POST /performers: Accepts performer data and creates a new performer.
+router.get('/:movieid/performers/:perfmerId', movieCtrl.addPerformer)
+
+
 module.exports = router 
